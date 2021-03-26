dcacdb329d07055b5473233188a7eff2c741f056
khala-zz <dokhaclam@gmail.com>
2021-03-26 10:58:20 +0700

    Update DatabaseSeeder.php

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -17,12 +17,52 @@ class DatabaseSeeder extends Seeder
         // $this->call(UsersTableSeeder::class);
 
         
+         // inserting document types
+        foreach (config('seed_data.document_types') as $value) {
 
+            DB::table('document_type')->insert([
+                'name' => $value
+            ]);
+        }
+
+        // insert task status
+        foreach (config('seed_data.task_statuses') as $value) {
+
+            DB::table('task_status')->insert([
+                'name' => $value
+            ]);
+        }
+
+        // insert task types
+        foreach (config('seed_data.task_types') as $value) {
+
+            DB::table('task_type')->insert([
+                'name' => $value
+            ]);
+        }
+
+        // insert contact status
+        foreach (config('seed_data.contact_status') as $value) {
+
+            DB::table('contact_status')->insert([
+                'name' => $value
+            ]);
+        }
+
+
+        // insert the system main email into settings table
+        foreach (config('seed_data.settings') as $key => $value) {
+            DB::table('setting')->insert([
+                'setting_key' => $key,
+                'setting_value' => $value
+            ]);
+        }
         // insert sample user as the system admin
         DB::table('users')->insert([
            'name' => 'admin',
            'email' => 'dokhaclam@gmail.com',
-           'position_title' => 'sales manager',
+            'password' => bcrypt("123456"),
+           'position_title' => 'Administrator',
            'is_admin' => 1,
            'is_active' => 1
         ]);
